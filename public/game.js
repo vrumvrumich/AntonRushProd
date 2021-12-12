e5484746b6de9522bada52f6f2091ef68079ea31
Yaroslav <rybkiy01@kits.kfplc.com>
2021-12-12 19:14:03 +0300

    feat: change game speed

--- public/game.js
+++ public/game.js
@@ -20,42 +20,62 @@ class Lighting {
     }
 }
 
-class Obstacles extends Object { }
+class Obstacle extends Object {
+    constructor(positionX, positionY, width, heigth, image) {
+        super(positionX, positionY, width, heigth)
+        this.image = image
+    }
+
+    setSpeed(speed) {
+        this.speed = speed
+        return this
+    }
+}
 
 class Player extends Object {
     constructor(positionX, positionY, width, heigth, idleImage, runImage) {
         super(positionX, positionY, width, heigth)
         this.idleImage = idleImage
         this.runImage = runImage
-        this.initialPosition = new Point(positionX,positionY)
+        this.initialPosition = new Point(positionX, positionY)
         this.jump = false
         this.fall = false
     }
 
-    setJumpHeigth(jumpHeigth){
+    setJumpHeigth(jumpHeigth) {
         this.jumpHeigth = jumpHeigth
         return this
     }
 
-    setInitialPosition(){
-        this.x = this.initialPosition.x
-        this.y = this.initialPosition.y
+    setPlayerSpeed(speed) {
+        this.speed = speed
+        return this
+    }
+
+    setInitialPosition() {
+        this.position.x = this.initialPosition.x
+        this.position.y = this.initialPosition.y
         this.jump = false
         this.fall = false
     }
 
+    setJumpSpeed(jumpSpeed){
+        this.jumpSpeed = jumpSpeed
+        return this
+    }
+
     performJump() {
         if (this.jump) {
-            if (this.position.y != this.initialPosition.y - this.jumpHeigth) this.position.y -= this.jumpHeigth/15
+            if (this.position.y != this.initialPosition.y - this.jumpHeigth) this.position.y -= this.jumpHeigth / 15
             else { this.fall = true; this.jump = false }
         }
         else if (this.fall) {
-            if (this.position.y != this.initialPosition.y) this.position.y += this.jumpHeigth/15
+            if (this.position.y != this.initialPosition.y) this.position.y += this.jumpHeigth / 15
             else this.fall = false
         }
     }
 
-    isInJumping(){
+    isInJumping() {
         return this.jump || this.fall
     }
 }
@@ -71,9 +91,9 @@ var gameOverAudios = [new Audio("game/relax_time.ogg"), new Audio("game/you're_s
 var gameOverImage = new Image()
 gameOverImage.src = "game/game_over.png"
 var obstacles = []
-const floor = 300
+const floor = 330
 
-var speed = 7
+var speed = 3
 var spawnTime = 1000;
 var game = false;
 var isGameStarted = false
@@ -87,8 +107,6 @@ var gameOverSoundIsEnded = true
 let player = preparePlayer()
 document.addEventListener('DOMContentLoaded', function () {
     var [background1, background2] = createBackgrounds()
-    var dino = new Image()
-    dino.src = "game/dino.png"
     time = performance.now()
     canvas.heigth = 400
     var center = new Point(canvas.width / 2, canvas.heigth / 2)
@@ -100,21 +118,23 @@ document.addEventListener('DOMContentLoaded', function () {
             drawMainEntities(canvasContext, background1, background2)
         }
         if (game) {
-            if (performance.now() - time > spawnTime) {
-                obstacles.push(new Obstacles(canvas.width - 70, canvas.heigth - 150, 70, 80))
+            if (performance.now() - time > 450 + Math.random()*spawnTime) {
                 time = performance.now()
+                createObstacle()
             }
             drawMainEntities(canvasContext, background1, background2)
             moveBackground(background1, background2)
             player.performJump()
             // drawLight()
-            obstacles.forEach((item, index) => {
-                canvasContext.drawImage(dino, item.position.x, item.position.y, item.width, item.heigth)
-                checkCollision(item)
-                item.position.x -= speed
-                if (item.position.x < -item.width / 2) {
-                    obstacles.splice(index, 1)
+            obstacles.forEach((obstacle) => {
+                canvasContext.drawImage(obstacle.image, obstacle.position.x, obstacle.position.y, obstacle.width, obstacle.heigth)
+                obstacle.position.x -= speed + player.speed
+                checkCollision(obstacle)
+                if (obstacle.position.x < -obstacle.width) {
+                    obstacles.pop()
                     score++
+                    speed += 0.05
+                    spawnTime -= 2
                 }
 
             }
@@ -148,14 +168,12 @@ document.body.onmousedown = function (e) {
 
 function checkCollision(obstacle) {
     if (obstacle.position.y > player.position.y && obstacle.position.y < player.position.y + player.heigth - 15
-        && obstacle.position.x > player.position.x+15 && obstacle.position.x < player.position.x + player.width-15) {
+        && obstacle.position.x > player.position.x + 15 && obstacle.position.x < player.position.x + player.width - 15) {
         gameOver()
     }
 }
 
 function gameOver() {
-    score = 0
-    obstacles.splice(0)
     let gameOverSound = gameOverAudios[Math.floor(Math.random() * gameOverAudios.length)]
     gameOverSound.play()
     gameOverSoundIsEnded = false
@@ -164,10 +182,13 @@ function gameOver() {
     gameOverSound.addEventListener("ended", function () {
         gameOverSoundIsEnded = true
         currentMainTheme.pause()
+        currentMainTheme.currentTime = 0
     });
 }
 
 function startGame() {
+    obstacles.splice(0)
+    score = 0
     currentMainTheme = mainThemeAudios[Math.floor(Math.random() * mainThemeAudios.length)]
     currentMainTheme.volume = 0.4
     currentMainTheme.play()
@@ -185,8 +206,8 @@ function createBackgrounds() {
 }
 
 function moveBackground(background1, background2) {
-    background1.position.x -= 5
-    background2.position.x -= 5
+    background1.position.x -= player.speed
+    background2.position.x -= player.speed
     if (background1.position.x <= -canvas.width) background1.position.x = canvas.width //+ (background1.positionX + canvas.width)
     if (background2.position.x <= -canvas.width) background2.position.x = canvas.width //+ (background2.positionX + canvas.width)
 }
@@ -197,7 +218,15 @@ function drawMainEntities(canvasContext, background1, background2) {
     canvasContext.drawImage(player.isInJumping() ? player.runImage : player.idleImage, player.position.x, player.position.y, player.width, player.heigth)
 }
 
+function createObstacle() {
+    obstacles.unshift(new Obstacle(canvas.width - 70, floor - 70, 70, 80, prepareObstacleImage()).setSpeed(speed))
+}
 
+function prepareObstacleImage() {
+    let obstacleImage = new Image()
+    obstacleImage.src = "game/dino.png"
+    return obstacleImage
+}
 
 function drawLight(canvasContext) {
     canvasContext.beginPath()
@@ -210,5 +239,5 @@ function preparePlayer() {
     let rushImage = new Image()
     idleImage.src = "game/main_character.png"
     rushImage.src = "game/rush.png"
-    return new Player(80, floor - 85, 100, 120, idleImage, rushImage).setJumpHeigth(165)
+    return new Player(80, floor - 113, 100, 120, idleImage, rushImage).setJumpHeigth(165).setJumpSpeed(15).setPlayerSpeed(8)
 }
