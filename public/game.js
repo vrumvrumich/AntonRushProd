ac29c8f283dd60cb0c38ea5b7adeb747883e6949
ribki <ribkin03@gmail.com>
2024-02-22 16:50:16 +0000

    fix: Refactor and fixes

--- public/game.js
+++ public/game.js
@@ -6,10 +6,10 @@ class Point {
 }
 
 class Object {
-    constructor(positionX, positionY, width, heigth) {
+    constructor(positionX, positionY, width, height) {
         this.position = new Point(positionX, positionY);
         this.width = width;
-        this.heigth = heigth
+        this.height = height
     }
 }
 
@@ -23,8 +23,8 @@ class Lighting {
 class Obstacles extends Object { }
 
 class Player extends Object {
-    constructor(positionX, positionY, width, heigth, idleImage, runImage) {
-        super(positionX, positionY, width, heigth)
+    constructor(positionX, positionY, width, height, idleImage, runImage) {
+        super(positionX, positionY, width, height)
         this.idleImage = idleImage
         this.runImage = runImage
         this.initialPosition = new Point(positionX,positionY)
@@ -32,8 +32,8 @@ class Player extends Object {
         this.fall = false
     }
 
-    setJumpHeigth(jumpHeigth){
-        this.jumpHeigth = jumpHeigth
+    setJumpheight(jumpheight){
+        this.jumpheight = jumpheight
         return this
     }
 
@@ -45,12 +45,12 @@ class Player extends Object {
     }
 
     performJump() {
-        if (this.jump) {
-            if (this.position.y != this.initialPosition.y - this.jumpHeigth) this.position.y -= this.jumpHeigth/15
+        if (this.jump ) {
+            if (this.position.y != this.initialPosition.y - this.jumpheight) this.position.y -= this.jumpheight/30
             else { this.fall = true; this.jump = false }
         }
         else if (this.fall) {
-            if (this.position.y != this.initialPosition.y) this.position.y += this.jumpHeigth/15
+            if (this.position.y != this.initialPosition.y) this.position.y += this.jumpheight/30
             else this.fall = false
         }
     }
@@ -61,8 +61,8 @@ class Player extends Object {
 }
 
 class Background extends Object {
-    constructor(positionX, positionY, width, heigth, image) {
-        super(positionX, positionY, width, heigth)
+    constructor(positionX, positionY, width, height, image) {
+        super(positionX, positionY, width, height)
         this.image = image
     }
 }
@@ -73,7 +73,7 @@ gameOverImage.src = "game/game_over.png"
 var obstacles = []
 const floor = 300
 
-var speed = 7
+var speed = document.documentElement.clientWidth/300
 var spawnTime = 1000;
 var game = false;
 var isGameStarted = false
@@ -88,11 +88,12 @@ let player = preparePlayer()
 document.addEventListener('DOMContentLoaded', function () {
     var [background1, background2] = createBackgrounds()
     var dino = new Image()
-    dino.src = "game/dino.png"
+    dino.src = "game/roza.png"
     time = performance.now()
-    canvas.heigth = 400
-    var center = new Point(document.documentElement.clientHe / 2, canvas.heigth / 2)
-    gameOverObject = new Object(center.x - (canvas.width / 4) / 2, center.y - (canvas.heigth / 2.5) / 2, canvas.width / 4, canvas.heigth / 2.5)
+    canvas.height = document.body.clientHeight
+    canvas.width = document.body.clientWidth
+    var center = new Point(document.documentElement.clientWidth / 2, document.documentElement.clientHeight / 2)
+    gameOverObject = new Object(center.x - (canvas.width / 4)/2, center.y - (canvas.height / 2.5)/2, canvas.width / 4, canvas.height / 2.5)
 
 
     function draw() {
@@ -100,16 +101,17 @@ document.addEventListener('DOMContentLoaded', function () {
             drawMainEntities(canvasContext, background1, background2)
         }
         if (game) {
+            // canvas.width = canvas.width
             if (performance.now() - time > spawnTime) {
-                obstacles.push(new Obstacles(canvas.width - 70, canvas.heigth - 150, 70, 80))
+                obstacles.push(new Obstacles(canvas.width - 70, document.documentElement.clientHeight/1.53, 110 , document.documentElement.clientHeight/5.1))
                 time = performance.now()
             }
-            drawMainEntities(canvasContext, background1, background2)
             moveBackground(background1, background2)
+            drawMainEntities(canvasContext, background1, background2)
             player.performJump()
             // drawLight()
             obstacles.forEach((item, index) => {
-                canvasContext.drawImage(dino, item.position.x, item.position.y, item.width, item.heigth)
+                canvasContext.drawImage(dino, item.position.x, item.position.y, item.width, item.height)
                 checkCollision(item)
                 item.position.x -= speed
                 if (item.position.x < -item.width / 2) {
@@ -147,54 +149,55 @@ document.body.onmousedown = function (e) {
 }
 
 function checkCollision(obstacle) {
-    if (obstacle.position.y > player.position.y && obstacle.position.y < player.position.y + player.heigth - 15
-        && obstacle.position.x > player.position.x+15 && obstacle.position.x < player.position.x + player.width-15) {
+    if (obstacle.position.y >= player.position.y && obstacle.position.y <= player.position.y + player.height - 50
+        && obstacle.position.x >= player.position.x && obstacle.position.x <= player.position.x + player.width-50 ) {
         gameOver()
     }
 }
 
 function gameOver() {
-    score = 0
     obstacles.splice(0)
     let gameOverSound = gameOverAudios[Math.floor(Math.random() * gameOverAudios.length)]
     gameOverSound.play()
     gameOverSoundIsEnded = false
     game = false
-    canvasContext.drawImage(gameOverImage, gameOverObject.position.x, gameOverObject.position.y, gameOverObject.width, gameOverObject.heigth)
+    canvasContext.drawImage(gameOverImage, gameOverObject.position.x, gameOverObject.position.y, gameOverObject.width, gameOverObject.height)
     gameOverSound.addEventListener("ended", function () {
         gameOverSoundIsEnded = true
         currentMainTheme.pause()
     });
 }
-
 function startGame() {
+    score = 0
+    player = preparePlayer()
     currentMainTheme = mainThemeAudios[Math.floor(Math.random() * mainThemeAudios.length)]
     currentMainTheme.volume = 0.4
     currentMainTheme.play()
     game = true
     isGameStarted = true
-    player.setInitialPosition()
 }
 
 function createBackgrounds() {
     let backgroudImage = new Image()
     backgroudImage.src = "game/back.jpg"
-    let background1 = new Background(0, 0, canvas.width, canvas.heigth, backgroudImage)
-    let background2 = new Background(canvas.width, 0, canvas.width, canvas.heigth, backgroudImage)
+    let background1 = new Background(0, 0, document.documentElement.clientWidth, document.documentElement.clientHeight, backgroudImage)
+    let background2 = new Background(document.documentElement.clientWidth, 0, document.documentElement.clientWidth, document.documentElement.clientHeight, backgroudImage)
     return [background1, background2]
 }
 
 function moveBackground(background1, background2) {
-    background1.position.x -= 5
-    background2.position.x -= 5
-    if (background1.position.x <= -canvas.width) background1.position.x = canvas.width //+ (background1.positionX + canvas.width)
-    if (background2.position.x <= -canvas.width) background2.position.x = canvas.width //+ (background2.positionX + canvas.width)
+    background1.position.x -= document.documentElement.clientWidth/400
+    background2.position.x -= document.documentElement.clientWidth/400
+    if (background1.position.x <= -document.documentElement.clientWidth)
+        background1.position.x = document.documentElement.clientWidth + background1.position.x + document.documentElement.clientWidth
+    if (background2.position.x <= -document.documentElement.clientWidth)
+        background2.position.x = document.documentElement.clientWidth + background2.position.x + document.documentElement.clientWidth
 }
 
 function drawMainEntities(canvasContext, background1, background2) {
-    canvasContext.drawImage(background1.image, background1.position.x, background1.position.y, background1.width, canvas.heigth)
-    canvasContext.drawImage(background2.image, background2.position.x, background2.position.y, background2.width, canvas.heigth)
-    canvasContext.drawImage(player.isInJumping() ? player.runImage : player.idleImage, player.position.x, player.position.y, player.width, player.heigth)
+    canvasContext.drawImage(background1.image, background1.position.x, background1.position.y, document.documentElement.clientWidth, document.documentElement.clientHeight)
+    canvasContext.drawImage(background2.image, background2.position.x, background2.position.y, document.documentElement.clientWidth, document.documentElement.clientHeight)
+    canvasContext.drawImage(player.isInJumping() ? player.runImage : player.idleImage, player.position.x, player.position.y, player.width, player.height)
 }
 
 
@@ -210,5 +213,5 @@ function preparePlayer() {
     let rushImage = new Image()
     idleImage.src = "game/main_character.png"
     rushImage.src = "game/rush.png"
-    return new Player(80, floor - 85, 100, 120, idleImage, rushImage).setJumpHeigth(165)
+    return new Player(80, (document.documentElement.clientHeight - document.documentElement.clientHeight/2.8), 120, document.documentElement.clientHeight/5, idleImage, rushImage).setJumpheight(180)
 }
